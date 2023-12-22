ac3bfc39912b789cdb2db7740d621a0ff90a1129
Tapishagrawal <tapishg4@gmail.com>
2023-12-23 03:40:23 +0530

    add basic functionality

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -2,10 +2,11 @@ import { Link } from "react-router-dom"
 
 interface ButtonProps {
     text: String,
-    link: any
+    link?: any,
+    handleClick?: () => void
 }
-export const Button: React.FC<ButtonProps> = ({ text, link }) => {
+export const Button: React.FC<ButtonProps> = ({ text, link, handleClick }) => {
     return (
-        <Link to={link} className="bg-indigo-500 hover:bg-indigo-600 py-3 px-4 rounded-md font-bold">{text}</Link>
+        <Link to={link} onClick={handleClick} className="bg-indigo-500 hover:bg-indigo-600 py-3 px-4 rounded-md font-bold">{text}</Link>
     )
 }
