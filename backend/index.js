703147263e9a6a6b14552d5e619a54126bc5c983
Tapishagrawal <tapishg4@gmail.com>
2023-12-22 14:16:18 +0530

    add cors in backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const connection = require('./connection');
 const { userRoute } = require('./Routes/user.route');
+const cors = require('cors');
 require("dotenv").config();
 
 const app = express();
+app.use(cors());
 app.use(express.json())
 
 app.use("/users", userRoute)
